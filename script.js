c71238067ffd19a7596ff7f4711d249fd4166841
Victor Lima <victord2704@gmail.com>
2023-10-23 22:05:47 -0300

    Att

--- script.js
+++ script.js
@@ -10,28 +10,38 @@ const temp = document.querySelector('.temperatura')
 const condImg = document.querySelector('.condicao-img')
 const condTipo = document.querySelector('.condicao-tipo')
 const umidade = document.querySelector('.umidade-p')
-const chuva = document.querySelector('.chuva-p')
 const vento = document.querySelector('.vento-km')
+const max = document.querySelector('.temp-max')
+const min = document.querySelector('.temp-min')
+const painelInfo = document.querySelector('.informacoes')
+const painelInfo2 = document.querySelector('.info-temp')
 
 //Funções
 const solicitaPrevisao = async (cidade) => { 
     const apiPrevisaoURL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${apiKey}&lang=pt_br`
     const res = await fetch(apiPrevisaoURL)
     const data = await res.json()
+    painelInfo.classList.remove('esconder')
+    painelInfo2.classList.remove('esconder')
 
     return data
 }
 
 const mostraPrevisao = async (cidade) => {
     const data = await solicitaPrevisao(cidade)
+    
 
     nomeCidade.innerText = data.name
     bandeira.src = `https://flagsapi.com/${data.sys.country}/flat/64.png`
     temp.innerHTML = `${parseInt(data.main.temp)}&deg`
     condImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
     condTipo.innerText = data.weather[0].description
-    
+    max.innerHTML = `${parseInt(data.main.temp_max)}&deg`
+    min.innerHTML = `${parseInt(data.main.temp_min)}&deg`
+    umidade.innerText = `${data.main.humidity}%`
+    vento.innerText = `${parseInt(data.wind.speed * 1.6)} km/h`
 }
+
 //Eventos
 
 botaoBuscar.addEventListener('click', (e) => {
