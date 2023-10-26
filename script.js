9a372e9a2dc50224d7191e73001f89aade6d4785
Victor Lima <victord2704@gmail.com>
2023-10-25 21:05:11 -0300

    Att

--- script.js
+++ script.js
@@ -21,31 +21,49 @@ const solicitaPrevisao = async (cidade) => {
     const apiPrevisaoURL = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${apiKey}&lang=pt_br`
     const res = await fetch(apiPrevisaoURL)
     const data = await res.json()
-    painelInfo.classList.remove('esconder')
-    painelInfo2.classList.remove('esconder')
+    
+    if (data.name === undefined) {
+        painelInfo.classList.add('esconder')
+        painelInfo2.classList.add('esconder')
+    } else {
+        painelInfo.classList.remove('esconder')
+        painelInfo2.classList.remove('esconder')
+    }
 
     return data
 }
 
 const mostraPrevisao = async (cidade) => {
     const data = await solicitaPrevisao(cidade)
-    
+    if (data.name === undefined) {
+        return
+    } else {
+        nomeCidade.innerText = data.name
+        bandeira.src = `https://flagsapi.com/${data.sys.country}/flat/64.png`
+        temp.innerHTML = `${parseInt(data.main.temp)}&deg`
+        condImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        condTipo.innerText = data.weather[0].description
+        max.innerHTML = `${parseInt(data.main.temp_max)}&deg`
+        min.innerHTML = `${parseInt(data.main.temp_min)}&deg`
+        umidade.innerText = `${data.main.humidity}%`
+        vento.innerText = `${parseInt(data.wind.speed * 1.6)} km/h`
+        cidadeInput.value = ''
+    }
 
-    nomeCidade.innerText = data.name
-    bandeira.src = `https://flagsapi.com/${data.sys.country}/flat/64.png`
-    temp.innerHTML = `${parseInt(data.main.temp)}&deg`
-    condImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-    condTipo.innerText = data.weather[0].description
-    max.innerHTML = `${parseInt(data.main.temp_max)}&deg`
-    min.innerHTML = `${parseInt(data.main.temp_min)}&deg`
-    umidade.innerText = `${data.main.humidity}%`
-    vento.innerText = `${parseInt(data.wind.speed * 1.6)} km/h`
 }
 
 //Eventos
 
 botaoBuscar.addEventListener('click', (e) => {
-    e.preventDefault()
     const cidade = cidadeInput.value
+    e.preventDefault()
     mostraPrevisao(cidade)
+})
+
+cidadeInput.addEventListener('keydown', (e) => {
+    const cidade = cidadeInput.value
+    if (e.key === 'Enter') {
+        e.preventDefault()
+        mostraPrevisao(cidade)
+    }
 })
